014238978c8a74afa0124cffee84ab84b8dd8dc1
Carlos Eduardo Carmo do Nascimento <karloseduardo19@hotmail.com>
2022-04-08 17:42:30 +0000

    changing films information

--- mobdev-app/src/app/pages/people/people.page.ts
+++ mobdev-app/src/app/pages/people/people.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-people',
   templateUrl: './people.page.html',
   styleUrls: ['./people.page.scss'],
 })
-export class PeoplePage implements OnInit {
+export class FilmsPage implements OnInit {
 
-  constructor() { }
+  films: Observable<any>;
+ 
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.films = this.http.get('https://swapi.dev/api/people');
+  }
+ 
+  openDetails(people) {
+    let split = people.url.split('/');
+    
+    this.router.navigateByUrl(`/tabs/people/${people}`);
   }
 
 }
