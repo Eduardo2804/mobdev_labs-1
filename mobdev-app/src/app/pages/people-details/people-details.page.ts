189b1687da3df01bdc836893423b9d87b5e2b3a3
Carlos Eduardo Carmo do Nascimento <karloseduardo19@hotmail.com>
2022-04-21 16:34:18 +0000

    creating planets details and people details in app

---
+++ mobdev-app/src/app/pages/people-details/people-details.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-people-details',
+  templateUrl: './people-details.page.html',
+  styleUrls: ['./people-details.page.scss'],
+})
+export class PeopleDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- mobdev-app/src/app/pages/people/people.page.ts
+++ mobdev-app/src/app/pages/people/people.page.ts
@@ -3,25 +3,26 @@ import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-people',
   templateUrl: './people.page.html',
   styleUrls: ['./people.page.scss'],
 })
-export class FilmsPage implements OnInit {
+export class PeoplePage implements OnInit {
 
-  films: Observable<any>;
+  people: Observable<any>;
  
   constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
-    this.films = this.http.get('https://swapi.dev/api/people');
+    this.people = this.http.get('https://swapi.dev/api/people');
   }
  
   openDetails(people) {
     let split = people.url.split('/');
-    
-    this.router.navigateByUrl(`/tabs/people/${people}`);
+    let peopleID = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/people/${peopleID}`);
   }
 
 }
